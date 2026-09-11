63f43b4daabd1621c15057312d702c1dea3e0dad
Pavel Vachek <ja@pvachek.cz>
2026-09-11 11:54:31 +0200

    Generate a single sitemap.xml without redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
+import sitemap from './src/integrations/sitemap';
 
 export default defineConfig({
   site: 'https://wayf.cz',
